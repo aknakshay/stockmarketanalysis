df282d421f45a024436a3ceb7f355abec3a1ff20
aknakshay <34308903+aknakshay@users.noreply.github.com>
2018-04-11 23:57:56 +0530

    Create TwitterSourceConstants.java

---
+++ PullData/flume.twitter.cloudsigma/flume.twitter/src/main/java/com/cloudsigma/flume/twitter/TwitterSourceConstants.java
@@ -0,0 +1,20 @@
+package com.cloudsigma.flume.twitter;
+
+public class TwitterSourceConstants {
+
+	  public static final String CONSUMER_KEY_KEY = "consumerKey";
+	  public static final String CONSUMER_SECRET_KEY = "consumerSecret";
+	  public static final String ACCESS_TOKEN_KEY = "accessToken";
+	  public static final String ACCESS_TOKEN_SECRET_KEY = "accessTokenSecret";
+	  
+	  public static final String BATCH_SIZE_KEY = "batchSize";
+	  public static final long DEFAULT_BATCH_SIZE = 1000L;
+
+	  public static final String KEYWORDS_KEY = "keywords";
+	  public static final String LANGUAGE_KEY = "language";
+	  public static final String FOLLOW_IDS_KEY = "follow";
+	  public static final String LOCATIONS_KEY = "locations";
+	  public static final String COUNT_PREV_TWEETS_KEY  = "count";
+	  
+	}
+
